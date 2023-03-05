a83fe294062edd8f5eaf3fe7fac390574a0a548f
bouhadiomar03@gmail.com <bouhadiomar03@gmail.com>
2023-03-05 21:27:43 +0100

    adding login page

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
 echo "Homepage for the employee management system."
 ?>
-<a href="dbconnect.php">Go to other page</a>
+<a href="dbconnect.php">Go to database connection page</a>
+<a href="login.php">Go to login page</a>
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Log In</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <form class="d-flex flex-column p-2 m-auto w-25 my-auto align-middle">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Id: </label>
+            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Id">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password: </label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+</html>
