29df8f99b35fcb6b2f34aa1d3329a98d43b17096
bouhadiomar03@gmail.com <bouhadiomar03@gmail.com>
2023-03-06 10:35:14 +0100

    added changepassword page for soufiane to handle

--- dashboard.php
+++ dashboard.php
@@ -6,19 +6,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
-// $query = "SELECT emp_no, password FROM dept_manager WHERE emp_no = ?";
-// $stmt = $db-> prepare($query);
-
-// $stmt->bind_param("s", $_SESSION["id"]);
-// $stmt->execute();
-
-// $result = $stmt->get_result();
-
-// foreach($result as $row){
-//     echo $row["password"];
-// }
-
 echo "You are logged in, this is the dashboard.<br>";
 echo "Your ID is: ".$_SESSION["id"]."<br>";
 ?>
 <a href="logout.php">Logout</a>
+<a href="changepassword.php">Change password</a>
