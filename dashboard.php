c8900e302253fd8084020a52b53429e857fb3c1b
bouhadiomar03@gmail.com <bouhadiomar03@gmail.com>
2023-03-06 17:23:03 +0100

    adding first version of employee table

--- dashboard.php
+++ dashboard.php
@@ -10,4 +10,7 @@ echo "You are logged in, this is the dashboard.<br>";
 echo "Your ID is: ".$_SESSION["id"]."<br>";
 ?>
 <a href="logout.php">Logout</a>
+<br>
 <a href="changepassword.php">Change password</a>
+<br>
+<a href="test_dashboard.php">Go to test dashboard</a>
